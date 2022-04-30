37a8983fecc41025bdabfde6593d26b25aeff57f
Andrew Moraes <andrewmoraesds@gmail.com>
2022-04-30 15:27:19 -0300

    initial commit with dal layer

---
+++ AltenHotel.API/Controllers/BookingController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AltenHotel.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        // GET: api/<BookingController>
+        [HttpGet]
+        public IEnumerable<string> GetAllAvailability()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<BookingController>/5
+        [HttpGet("{date}")]
+        public string GetSingleDate(DateTime date)
+        {
+            return "value";
+        }
+
+        // POST api/<BookingController>
+        [HttpPost]
+        public void PlaceReservation([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BookingController>/5
+        [HttpPut("{id}")]
+        public void EditReservation(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BookingController>/5
+        [HttpDelete("{id}")]
+        public void CancelReservation(int id)
+        {
+        }
+    }
+}
---
+++ AltenHotel.API/Infra/DAL/DBCommunication.cs
@@ -0,0 +1,116 @@
+﻿using AltenHotel.API.Infra.Factories;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AltenHotel.API.Infra.DAL
+{
+    public class DBCommunication
+    {
+        private ConnectionFactory _connection;
+        public DBCommunication()
+        {
+            _connection = new ConnectionFactory();
+        }
+
+        /// <summary>
+        /// Executes a procedure that will make operations on the database, i.e insert, update or delete.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public dynamic ExecuteOperation(string name, Dictionary<string, object> param)
+        {
+            var result = _connection.ExecuteScalar(name, CommandType.StoredProcedure, param);
+            return result;
+        }
+
+        /// <summary>
+        /// Executes and returns select operations.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public IEnumerable<T> ExecuteGet<T>(string name, Dictionary<string, dynamic> param)
+        {
+            DataTable dt = new DataTable();
+            var sqlResult = _connection.GetReader(name, CommandType.StoredProcedure, param);
+            dt.Load(sqlResult);
+
+            var result = TranslateDataTable<T>(dt);
+            return result;
+        }
+
+        /// <summary>
+        /// Make the transformation of the DB result for a object from a class inside the code.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public IEnumerable<T> TranslateDataTable<T>(dynamic db)
+        {
+            var dt = (DataTable)db;
+
+            var columns = dt.Columns.Cast<DataColumn>().Select(d => new { d.DataType, d.ColumnName }).ToList();
+            var result = new List<T>();
+
+            foreach (DataRow item in dt.Rows)
+            {
+                var entity = Activator.CreateInstance(typeof(T));
+
+                foreach (var column in columns)
+                {
+                    var type = Type.GetTypeCode(column.DataType);
+                    var value = item[column.ColumnName];
+
+                    if (type == TypeCode.UInt64)
+                    {
+                        if (value == null || DBNull.Value.Equals(value))
+                        {
+                            value = false;
+                        }
+                        else
+                        {
+                            value = Convert.ToBoolean(value);
+                        }
+
+                    }
+
+                    if (value == null || DBNull.Value.Equals(value))
+                    {
+                        value = ValidateNullValue(column.DataType.Name);
+                    }
+
+                    entity?.GetType()?.GetProperty(column.ColumnName).SetValue(entity, value);
+                }
+                result.Add((T)entity);
+            }
+
+            return result;
+        }
+
+        private dynamic ValidateNullValue(string type)
+        {
+            switch (type)
+            {
+                case "Int32":
+                    return 0;
+
+                case "String":
+                    return "";
+
+                case "DateTime":
+                    return null;
+
+                case "Boolean":
+                    return false;
+
+                default:
+                    return null;
+            }
+        }
+    }
+}

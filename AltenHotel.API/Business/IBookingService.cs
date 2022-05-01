d99de4499d67bb154b5a5f4e19e0de731b5b07f1
Andrew Moraes <andrewmoraesds@gmail.com>
2022-05-01 16:24:22 -0300

    add interface IBookingService

---
+++ AltenHotel.API/Business/IBookingService.cs
@@ -0,0 +1,16 @@
+﻿using AltenHotel.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AltenHotel.API.Business
+{
+    interface IBookingService
+    {
+        public List<DateTime> GetAvailability();
+        public dynamic PlaceReservation(Reservation obj);
+        public dynamic UpdateReservation(Reservation obj);
+        public dynamic CancelReservation(int id);
+    }
+}
